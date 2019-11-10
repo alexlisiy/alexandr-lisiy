ec72c302f41972349f1c6512e5d76ff8660a29d3
Alexandr Lisiy <lysalex78@gmail.com>
2019-11-10 23:21:12 +0200

    Magento upgrade from 2.3.2 to 2.3.3 - homework

---
+++ app/etc/env.php
@@ -0,0 +1,74 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => '74f03ce991d732fec775cb4d5df7ff42'
+    ],
+    'db' => [
+        'table_prefix' => 'm2_',
+        'connection' => [
+            'default' => [
+                'host' => 'mysql',
+                'dbname' => 'alexandr_lisiy_local',
+                'username' => 'alexandr_lisiy_local',
+                'password' => 'alexandr_lisiy_local',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '40d_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '40d_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 0,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 0,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Sun, 06 Oct 2019 22:59:21 +0000'
+    ],
+    'downloadable_domains' => [
+        'alexandr-lisiy.local',
+        'site2.alexandr-lisiy.local'
+    ]
+];
---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,32 @@
+module.exports = {
+    alexandrl_luma_en_US: {
+        area: 'frontend',
+        name: 'Alexandrl/luma',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    alexandrl_luma_uk_UA: {
+        area: 'frontend',
+        name: 'Alexandrl/luma',
+        locale: 'uk_UA',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    alexandrl_luma_ru_RU: {
+        area: 'frontend',
+        name: 'Alexandrl/luma',
+        locale: 'ru_RU',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    }
+};
